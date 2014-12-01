d462022317e91ee9b07a56b00e3f39c71137863e
Rui Salgado <ruivfsalgado@gmail.com>
2014-12-01 16:43:17 +0000

    Validate that customer is at least 16 years old

---
+++ src/main/java/nl/yellowbrick/validation/GeneralCustomerValidator.java
@@ -0,0 +1,31 @@
+package nl.yellowbrick.validation;
+
+import nl.yellowbrick.domain.Customer;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+@Component
+public class GeneralCustomerValidator extends AccountRegistrationValidator {
+
+    @Override
+    protected void validate(Customer customer, Errors errors) {
+        if(customer.getDateOfBirth() == null) {
+            errors.rejectValue("dateOfBirth", "missing");
+            return;
+        }
+
+        boolean youngerThanSixteen = sixteenYearsAgo().isBefore(Instant.ofEpochMilli(customer.getDateOfBirth().getTime()));
+
+        if(youngerThanSixteen) {
+            errors.rejectValue("dateOfBirth", "too.young");
+        }
+    }
+
+    private Instant sixteenYearsAgo() {
+        return LocalDate.now().minusYears(16).atStartOfDay(ZoneId.systemDefault()).toInstant();
+    }
+}
