4a90c6c84c680944894e7d3e65097d809b41ff8c
Rui Salgado <ruivfsalgado@gmail.com>
2014-12-04 16:36:25 +0000

    Add initial, crude styling based on James’ wireframes
    Create custom thymeleaf dialect and use it to 
    determine the active section on the side menu.

--- modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/controller/AccountProvisioningController.java
+++ modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/controller/AccountProvisioningController.java
@@ -6,19 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloWorldController {
+@RequestMapping("/provisioning")
+public class AccountProvisioningController {
 
     @Autowired
     private CustomerDao customerDao;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @RequestMapping(method = RequestMethod.GET)
+    public String pendingValidation(Model model) {
         model.addAttribute("customers", customerDao.findAllPendingActivation());
 
-        return "hello";
+        return "provisioning/index";
     }
 }
---
+++ modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/dialect/BrickwallDialect.java
@@ -0,0 +1,26 @@
+package nl.yellowbrick.admin.dialect;
+
+import org.springframework.stereotype.Component;
+import org.thymeleaf.dialect.AbstractDialect;
+import org.thymeleaf.processor.IProcessor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class BrickwallDialect extends AbstractDialect {
+
+  public String getPrefix() {
+    return "bw";
+  }
+
+  @Override
+  public Set<IProcessor> getProcessors() {
+    Set<IProcessor> processors = new HashSet<>();
+
+    processors.add(new ActiveForContextAttrProcessor());
+
+    return processors;
+  }
+
+}
