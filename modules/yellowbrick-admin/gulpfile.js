049061cf5f4abf8b193fdc68964e9e07d9c1e1ed
Rui Salgado <ruivfsalgado@gmail.com>
2014-12-17 12:57:42 +0000

    Make “gulp watch” resilient to errors

--- modules/yellowbrick-admin/gulpfile.js
+++ modules/yellowbrick-admin/gulpfile.js
@@ -6,9 +6,23 @@ var paths = {
     destination: './target/classes/static'
 };
 
+var displayError = function (err) {
+    var errorString = '[' + err.plugin + ']';
+    errorString += ' ' + err.message.replace("\n",''); // Removes new line at the end
+
+    if(err.fileName)
+        errorString += ' in ' + err.fileName;
+
+    if(err.lineNumber)
+        errorString += ' on line ' + err.lineNumber;
+
+    console.error(errorString);
+};
+
 gulp.task('sass', function () {
     gulp.src(paths.scss)
         .pipe(sass())
+        .on('error', displayError)
         .pipe(gulp.dest(paths.destination + '/css'));
 });
 
