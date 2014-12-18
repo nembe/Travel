5a5d0edb436cabd509e5bf3343e18eef2cc3b4f3
Rui Salgado <ruivfsalgado@gmail.com>
2014-12-18 08:45:08 +0000

    Cleanup test database setup
    Remove custom config from yaml files as the 
    default from spring is fine.
    Add cleanup script which runs before anything else.

--- modules/yellowbrick-data/src/test/java/nl/yellowbrick/data/TestConfiguration.java
+++ modules/yellowbrick-data/src/test/java/nl/yellowbrick/data/TestConfiguration.java
@@ -20,7 +20,7 @@ public class TestConfiguration {
         return new EmbeddedDatabaseBuilder()
                 .setDataSourceFactory(new SingleConnectionDataSourceFactory())
                 .setType(EmbeddedDatabaseType.HSQL)
-                .addScript("functions.sql")
+                .addScripts("cleanup.sql", "functions.sql")
                 .build();
     }
 }
