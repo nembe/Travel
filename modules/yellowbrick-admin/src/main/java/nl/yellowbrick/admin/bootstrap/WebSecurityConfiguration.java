5d1044b940a51cf5899c51ff5025826136e39810
Rui Salgado <ruivfsalgado@gmail.com>
2015-01-05 22:07:57 +0000

    Externalise ldap settings

--- modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/bootstrap/WebSecurityConfiguration.java
+++ modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/bootstrap/WebSecurityConfiguration.java
@@ -1,7 +1,10 @@
 package nl.yellowbrick.admin.bootstrap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,14 +29,17 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
+    @Bean
+    @ConfigurationProperties(prefix = "ldap")
+    public LdapContextSource ldapContextSource() {
+        return new LdapContextSource();
+    }
+
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder auth, LdapContextSource ldapContextSource) throws Exception {
         auth.ldapAuthentication()
                 .userSearchBase("OU=BP Users,OU=Waysis - BP,DC=waysis,DC=local")
                 .userSearchFilter("(sAMAccountName={0})")
-                .contextSource()
-                    .url("ldaps://vm-dc-01:636")
-                    .managerDn("CN=ldap_user,CN=Users,DC=waysis,DC=local")
-                    .managerPassword("ldap_user");
+                .contextSource(ldapContextSource);
     }
 }
