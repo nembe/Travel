2f40b8796f42acf1829eb0f716f7c1c0718900f3
Rui Salgado <ruivfsalgado@gmail.com>
2015-01-06 14:16:20 +0000

    Fix issue with some pages not requiring authenticated user

--- modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/bootstrap/WebSecurityConfiguration.java
+++ modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/bootstrap/WebSecurityConfiguration.java
@@ -17,16 +17,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
             .formLogin()
                 .loginPage("/login")
                 .permitAll()
                 .and()
             .logout()
-                .permitAll();
+                .permitAll()
+                .and()
+            .authorizeRequests()
+                .anyRequest()
+                .authenticated();
     }
 
     @Bean
