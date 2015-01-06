2dd97373f5e7fb5b96be2e8b29ba66b70322a681
Rui Salgado <ruivfsalgado@gmail.com>
2015-01-06 14:46:31 +0000

    Ensure ldap server is trusted regardless of how app is booted

--- modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/bootstrap/Application.java
+++ modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/bootstrap/Application.java
@@ -4,11 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.net.URL;
+
 @ComponentScan(basePackages = { "nl.yellowbrick" })
 @EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
+        setSslEnvironment();
         SpringApplication.run(Application.class, args);
+    }
+
+    public static void setSslEnvironment() {
+        URL trustStoreUrl = Application.class.getClassLoader().getResource("truststore.jks");
+
+        System.setProperty("javax.net.ssl.trustStore", trustStoreUrl.getPath());
+        System.setProperty("javax.net.ssl.trustStoreType", "jks");
     }
 }
