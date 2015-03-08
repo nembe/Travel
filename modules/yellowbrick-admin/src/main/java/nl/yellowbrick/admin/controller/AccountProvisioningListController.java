e52af2e15ad46323885a5d72a15e827cac5a9d34
Rui Salgado <ruivfsalgado@gmail.com>
2015-03-08 23:14:59 +0000

    Filter customers pending validation by status

--- modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/controller/AccountProvisioningListController.java
+++ modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/controller/AccountProvisioningListController.java
@@ -10,13 +10,17 @@ import nl.yellowbrick.data.domain.ProductGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("/provisioning/accounts")
@@ -25,41 +29,76 @@ public class AccountProvisioningListController {
     @Autowired private CustomerDao customerDao;
     @Autowired private ProductGroupDao productGroupDao;
 
-    @RequestMapping(method = RequestMethod.GET, params = { "productGroup" })
-    public String accountsPendingValidation(ModelMap model, @RequestParam("productGroup") long productGroupId) {
-        List<ProductGroup> allProductGroups = productGroupDao.all();
-        ProductGroup selectedProductGroup = allProductGroups.stream()
-                .filter(pg -> pg.getId() == productGroupId)
-                .findFirst()
-                .orElseThrow(() -> new ResourceNotFoundException("could not find product group with id " + productGroupId));
+    @ModelAttribute("allProductGroups")
+    public List<ProductGroup> allProductGroups() {
+        return productGroupDao.all();
+    }
 
-        return accountsPendingValidation(model, allProductGroups, selectedProductGroup);
+    @ModelAttribute("allStatuses")
+    public Map<String, StatusFilter> statuses() {
+        return new HashMap<String, StatusFilter>() {{
+            put("0", StatusFilter.ANY);
+            put("1", StatusFilter.FAILED);
+            put("2", StatusFilter.REGISTERED);
+        }};
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String accountsPendingValidation(ModelMap model) {
-        List<ProductGroup> allProductGroups = productGroupDao.all();
-        ProductGroup defaultProductGroup = allProductGroups.stream()
-                .findFirst()
-                .orElseThrow(() -> new InconsistentDataException("no product groups"));
+    public String accountsPendingValidation(ModelMap model,
+                                            @RequestParam Map<String, String> requestParams,
+                                            @ModelAttribute("allProductGroups") List<ProductGroup> allProductGroups,
+                                            @ModelAttribute("allStatuses") Map<String, StatusFilter> allStatuses) {
 
-        return accountsPendingValidation(model, allProductGroups, defaultProductGroup);
-    }
+        ProductGroup productGroup = requestedProductGroupOrDefault(requestParams, allProductGroups);
+        StatusFilter statusFilter = requestedStatusFilterOrDefault(requestParams, allStatuses);
 
-    private String accountsPendingValidation(ModelMap model,
-                                             List<ProductGroup> allProductGroups,
-                                             ProductGroup selectedProductGroup) {
-        model.addAttribute("allProductGroups", allProductGroups);
-        model.addAttribute("productGroup", selectedProductGroup);
-        model.addAttribute("customers", customersPendingManualValidation()
-                .filter(customer -> customer.getProductGroupId() == selectedProductGroup.getId())
-                .collect(Collectors.toList()));
+        model.addAttribute("productGroup", productGroup);
+        model.addAttribute("statusFilter", statusFilter);
+        model.addAttribute("customers", customersPendingActivation(productGroup, statusFilter));
 
         return "provisioning/accounts/index";
     }
 
-    private Stream<Customer> customersPendingManualValidation() {
+    private StatusFilter requestedStatusFilterOrDefault(Map<String, String> requestParams,
+                                                        Map<String, StatusFilter> allStatuses) {
+        if(!requestParams.containsKey("statusFilter"))
+            return StatusFilter.ANY;
+
+        return allStatuses.getOrDefault(requestParams.get("statusFilter"), StatusFilter.ANY);
+    }
+
+    private ProductGroup requestedProductGroupOrDefault(Map<String, String> requestParams,
+                                                        List<ProductGroup> allProductGroups) {
+
+        if(!requestParams.containsKey("productGroup"))
+            return allProductGroups.stream()
+                    .findFirst()
+                    .orElseThrow(() -> new InconsistentDataException("no product groups"));
+
+        String requestedProductGroup = requestParams.get("productGroup");
+
+        return allProductGroups.stream()
+                .filter(pg -> pg.getId().toString().equals(requestedProductGroup))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("unknown product group id " + requestedProductGroup));
+    }
+
+    private List<Customer> customersPendingActivation(ProductGroup productGroup, StatusFilter statusFilter) {
         return customerDao.findAllPendingActivation().stream()
-                .filter(customer -> customer.getCustomerStatusIdfk() == CustomerStatus.ACTIVATION_FAILED.code());
+                .filter(customer -> customer.getProductGroupId() == productGroup.getId())
+                .filter(statusFilter.filter)
+                .collect(Collectors.toList());
+    }
+
+    private enum StatusFilter {
+        ANY(c -> Arrays.asList(CustomerStatus.ACTIVATION_FAILED, CustomerStatus.REGISTERED).contains(c.getStatus())),
+        FAILED(c -> c.getStatus() == CustomerStatus.ACTIVATION_FAILED),
+        REGISTERED(c -> c.getStatus() == CustomerStatus.REGISTERED);
+
+        final Predicate<Customer> filter;
+
+        StatusFilter(Predicate<Customer> filter) {
+            this.filter = filter;
+        }
     }
 }
