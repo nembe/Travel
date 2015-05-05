6535d02b9b3afc87c56e1330476120305c25cd2c
Rui Salgado <ruivfsalgado@gmail.com>
2015-05-05 10:02:49 +0100

    Add BankingInfoValidator
    Which checks for existing accounts with same SEPA
    number as the newly registered account.

--- modules/yellowbrick-data/src/test/java/nl/yellowbrick/data/dao/impl/DirectDebitDetailsJdbcDaoTest.java
+++ modules/yellowbrick-data/src/test/java/nl/yellowbrick/data/dao/impl/DirectDebitDetailsJdbcDaoTest.java
@@ -6,13 +6,18 @@ import nl.yellowbrick.data.domain.DirectDebitDetails;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class DirectDebitDetailsJdbcDaoTest extends BaseSpringTestCase {
+
+    private static final long CUSTOMER_ID_WITH_DATA = 394744;
+    private static final long CUSTOMER_ID_WITHOUT_DATA = 12345;
 
     @Autowired
     DirectDebitDetailsJdbcDao dao;
@@ -21,21 +26,20 @@ public class DirectDebitDetailsJdbcDaoTest extends BaseSpringTestCase {
     DbHelper db;
 
     @Test
-    public void returns_empty_for_no_match() {
-        assertThat(dao.findForCustomer(12345), equalTo(Optional.empty()));
+    public void finds_direct_debit_details_by_customer_id() {
+        DirectDebitDetails details = dao.findForCustomer(CUSTOMER_ID_WITH_DATA).get();
+
+        assertThat(details, equalTo(expectedDetails()));
+
+        assertThat(dao.findForCustomer(CUSTOMER_ID_WITHOUT_DATA), equalTo(Optional.empty()));
     }
 
     @Test
-    public void returns_details_otherwise() {
-        DirectDebitDetails expectation = new DirectDebitDetails();
-        expectation.setId(1);
-        expectation.setBic("RABONL2U");
-        expectation.setSepaNumber("NL39 RABO 0300 0652 64");
-        expectation.setVerified(true);
+    public void finds_direct_debit_details_by_sepa_number() {
+        List<DirectDebitDetails> detailsList = dao.findBySepaNumber("NL39 RABO 0300 0652 64");
 
-        DirectDebitDetails details = dao.findForCustomer(394744).get();
-
-        assertThat(details, equalTo(expectation));
+        assertThat(detailsList, hasSize(1));
+        assertThat(detailsList.get(0), equalTo(expectedDetails()));
     }
 
     @Test
@@ -45,5 +49,15 @@ public class DirectDebitDetailsJdbcDaoTest extends BaseSpringTestCase {
 
         db.accept((t) -> t.update("UPDATE PAYMENT_DIRECT_DEBIT_DETAILS SET VERIFIED = 'Y'"));
         assertThat(dao.findForCustomer(394744).get().isVerified(), is(true));
+    }
+
+    private DirectDebitDetails expectedDetails() {
+        DirectDebitDetails details = new DirectDebitDetails();
+        details.setId(1);
+        details.setBic("RABONL2U");
+        details.setSepaNumber("NL39 RABO 0300 0652 64");
+        details.setVerified(true);
+
+        return details;
     }
 }
