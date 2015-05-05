6dabf10b529e02d11c0f4cf5afe70d45ed477554
Rui Salgado <ruivfsalgado@gmail.com>
2015-05-05 14:52:57 +0100

    Refactor GeneralCustomerValidator
    Make it more declarative

--- modules/yellowbrick-account-activation/src/main/java/nl/yellowbrick/activation/validation/GeneralCustomerValidator.java
+++ modules/yellowbrick-account-activation/src/main/java/nl/yellowbrick/activation/validation/GeneralCustomerValidator.java
@@ -25,6 +25,37 @@ public class GeneralCustomerValidator extends AccountRegistrationValidator {
 
     @Override
     protected void doValidate(Customer customer, Errors errors) {
+        validateAge(customer, errors);
+        validateActionCode(customer, errors);
+        validateRegistrationMobile(customer, errors);
+    }
+
+    private void validateRegistrationMobile(Customer customer, Errors errors) {
+        if(customer.getFirstCardMobile() == null)
+            return;
+
+        String mobile = customer.getFirstCardMobile().replaceAll("\\s", "");
+
+        customerDao.findAllByMobile(mobile)
+                .stream()
+                .filter(it -> it.getCustomerId() != customer.getCustomerId())
+                .findAny()
+                .ifPresent(it -> errors.rejectValue("firstCardMobile", "errors.duplicate"));
+    }
+
+    private void validateActionCode(Customer customer, Errors errors) {
+        if(Strings.isNullOrEmpty(customer.getActionCode()))
+            return;
+
+        boolean validActionCode = marketingActionDao.findByActionCode(customer.getActionCode())
+                .map(MarketingAction::isCurrentlyValid)
+                .orElse(false);
+
+        if(!validActionCode)
+            errors.rejectValue("actionCode", "errors.invalid.action.code");
+    }
+
+    private void validateAge(Customer customer, Errors errors) {
         if(customer.getDateOfBirth() == null) {
             errors.rejectValue("dateOfBirth", "errors.missing");
             return;
@@ -32,31 +63,6 @@ public class GeneralCustomerValidator extends AccountRegistrationValidator {
 
         if(youngerThanSixteen(customer.getDateOfBirth()))
             errors.rejectValue("dateOfBirth", "errors.too.young");
-
-        if(!Strings.isNullOrEmpty(customer.getActionCode()) && !validActionCode(customer.getActionCode()))
-            errors.rejectValue("actionCode", "errors.invalid.action.code");
-
-        if(registeredWithKnownMobile(customer))
-            errors.rejectValue("firstCardMobile", "errors.duplicate");
-    }
-
-    private boolean registeredWithKnownMobile(Customer customer) {
-        if(customer.getFirstCardMobile() == null)
-            return false;
-
-        String mobile = customer.getFirstCardMobile().replaceAll("\\s", "");
-
-        return customerDao.findAllByMobile(mobile)
-                .stream()
-                .filter(it -> it.getCustomerId() != customer.getCustomerId())
-                .findAny()
-                .isPresent();
-    }
-
-    private boolean validActionCode(String actionCode) {
-        return marketingActionDao.findByActionCode(actionCode)
-                .map(MarketingAction::isCurrentlyValid)
-                .orElse(false);
     }
 
     private boolean youngerThanSixteen(Date dateOfBirth) {
