7b0895a2d184a411ab7bfe563f0e3dd7bbb475f9
Rui Salgado <ruivfsalgado@gmail.com>
2015-05-18 11:37:01 +0100

    Avoid spamming the admin with alerts
    Won’t receive the same email twice within a 6 
    hours period.

--- modules/yellowbrick-account-activation/src/main/java/nl/yellowbrick/activation/service/AdminNotificationService.java
+++ modules/yellowbrick-account-activation/src/main/java/nl/yellowbrick/activation/service/AdminNotificationService.java
@@ -1,5 +1,6 @@
 package nl.yellowbrick.activation.service;
 
+import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,19 @@ import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.function.Function;
+
+import static java.time.LocalDateTime.*;
 
 @Component
 public class AdminNotificationService {
+
+    // keep track of when messages are sent to avoid spamming the admin
+    private static final HashMap<Integer, LocalDateTime> MESSAGES_SENT_TIMES = Maps.newHashMap();
+    // make sure we don't send the same message twice within a certain time window
+    private static final int MINUTES_BETWEEN_REPEATS = 6 * 60;
 
     private static final String FROM = "noreply@brickparking.com";
 
@@ -25,7 +35,7 @@ public class AdminNotificationService {
 
     private final String profile;
     private final String adminEmail;
-    private final JavaMailSender mailSender;
+    private JavaMailSender mailSender;
 
     private Logger log = LoggerFactory.getLogger(AdminNotificationService.class);
 
@@ -40,19 +50,45 @@ public class AdminNotificationService {
         this.mailSender = mailSender;
     }
 
+    public void setMailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     public void notifyCardPoolExhausted(int productGroupId) {
         try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "ISO-8859-1");
+            MimeMessage message = createMessage(
+                    POOL_EXHAUSTED_SUBJECT.apply(profile),
+                    POOL_EXHAUSTED_BODY.apply(String.valueOf(productGroupId))
+            );
 
-            messageHelper.setFrom(FROM);
-            messageHelper.setTo(adminEmail);
-            messageHelper.setSubject(POOL_EXHAUSTED_SUBJECT.apply(profile));
-            messageHelper.setText(POOL_EXHAUSTED_BODY.apply(String.valueOf(productGroupId)), false);
-
-            mailSender.send(message);
+            sendMessage(message);
         } catch (MessagingException e) {
             log.error("Failed to compose notifyCardPoolExhausted email", e);
         }
+    }
+
+    private MimeMessage createMessage(String subject, String body) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "ISO-8859-1");
+
+        messageHelper.setFrom(FROM);
+        messageHelper.setTo(adminEmail);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(body, false);
+
+        return message;
+    }
+
+    private void sendMessage(MimeMessage message) throws MessagingException {
+        int msgHash = message.hashCode();
+
+        if(MESSAGES_SENT_TIMES.containsKey(msgHash)
+                && MESSAGES_SENT_TIMES.get(msgHash).isAfter(now().minusMinutes(MINUTES_BETWEEN_REPEATS))) {
+            log.warn("skipping sending message {} to avoid spamming email", message.getSubject());
+            return;
+        }
+
+        mailSender.send(message);
+        MESSAGES_SENT_TIMES.put(msgHash, now());
     }
 }
