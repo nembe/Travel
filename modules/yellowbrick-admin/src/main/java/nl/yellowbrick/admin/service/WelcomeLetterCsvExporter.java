9e7ccbd5efc1a29c00dc06c5adc9b89fff6a5ade
Rui Salgado <ruivfsalgado@gmail.com>
2015-05-20 22:40:28 +0100

    Add CSV exporter for welcome letters based on card order exporter
    As requested the CSV keeps the same columns as 
    the Card Order export but only fills in the
    customer data (incl address).
    Can export customers with customer ID higher than
    a provided ID or between a date range.
    Some performance considerations were made to avoid
    OOM errors if for some reason someone decides to 
    export an incredibly large number of customers.

---
+++ modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/service/WelcomeLetterCsvExporter.java
@@ -0,0 +1,131 @@
+package nl.yellowbrick.admin.service;
+
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.dataformat.csv.CsvGenerator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.google.common.base.Joiner;
+import nl.yellowbrick.admin.domain.CardOrderExportRecord;
+import nl.yellowbrick.data.domain.ProductGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class WelcomeLetterCsvExporter {
+
+    private static final char SEPARATOR = ';';
+
+    private Path baseExportPath;
+
+    @Autowired
+    public WelcomeLetterCsvExporter(@Value("${welcomeLetterExport.path}") String baseExportPath) {
+        setBaseExportPath(baseExportPath);
+    }
+
+    public void setBaseExportPath(String baseExportPath) {
+        this.baseExportPath = Paths.get(baseExportPath).toAbsolutePath();
+    }
+
+    public Appender createAppender(ProductGroup productGroup, String name) {
+        try {
+            return new Appender(resolveFilePath(productGroup, name));
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Path resolveFilePath(ProductGroup productGroup, String name) throws IOException {
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmm"));
+        Path dir = exportsDir(productGroup);
+
+        String filename = Joiner
+                .on("_").join(time, productGroup.getDescription(), name)
+                .concat(".csv")
+                .replaceAll("\\s", "");
+
+        return dir.resolve(filename);
+    }
+
+    private Path exportsDir(ProductGroup productGroup) throws IOException {
+        Path path = baseExportPath.resolve(productGroup.getId().toString());
+
+        return path.toFile().exists()
+                ? path
+                : Files.createDirectory(path);
+    }
+
+    public static class Appender implements AutoCloseable {
+
+        private enum State {
+            INITIAL, WRITING, CLOSED
+        }
+
+        private final Path path;
+        private ObjectWriter writer;
+        private CsvGenerator generator;
+        private State state;
+
+        private Appender(Path path) {
+            this.path = path;
+            this.state = State.INITIAL;
+        }
+
+        public void append(CardOrderExportRecord record) {
+            if(state == State.CLOSED)
+                throw new IllegalStateException("Appender has already been closed");
+
+            try {
+                if(state == State.INITIAL)
+                    initialize();
+
+                state = State.WRITING;
+                writer.writeValue(generator, record);
+            } catch(IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public boolean isWriting() {
+            return state == State.WRITING;
+        }
+
+        public void close() {
+            if(!isWriting())
+                return;
+
+            try {
+                generator.flush();
+                generator.close();
+            } catch(IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                writer = null;
+                generator = null;
+            }
+        }
+
+        public Path getPath() {
+            return path;
+        }
+
+        private void initialize() throws IOException {
+            CsvMapper mapper = new CsvMapper();
+            CsvSchema schema = mapper.schemaFor(CardOrderExportRecord.class)
+                    .withHeader()
+                    .withoutQuoteChar()
+                    .withColumnSeparator(SEPARATOR);
+
+            writer = mapper.writer(schema);
+            generator = mapper.getFactory().createGenerator(new FileWriter(path.toFile()));
+        }
+    }
+}
---
+++ modules/yellowbrick-admin/src/main/java/nl/yellowbrick/admin/service/WelcomeLetterExportService.java
@@ -0,0 +1,67 @@
+package nl.yellowbrick.admin.service;
+
+import nl.yellowbrick.admin.domain.CardOrderExportRecord;
+import nl.yellowbrick.data.dao.CustomerAddressDao;
+import nl.yellowbrick.data.dao.CustomerDao;
+import nl.yellowbrick.data.domain.AddressType;
+import nl.yellowbrick.data.domain.Customer;
+import nl.yellowbrick.data.domain.CustomerAddress;
+import nl.yellowbrick.data.domain.ProductGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Path;
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+public class WelcomeLetterExportService {
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Autowired
+    private CustomerAddressDao customerAddressDao;
+
+    @Autowired
+    private WelcomeLetterCsvExporter csvExporter;
+
+    public Optional<Path> exportForProductGroup(ProductGroup productGroup, long fromCustomerIdExclusive) {
+        String exportName = String.format("from-customer-%s", fromCustomerIdExclusive);
+
+        try(WelcomeLetterCsvExporter.Appender appender = csvExporter.createAppender(productGroup, exportName)) {
+            customerDao.scan(
+                    productGroup,
+                    fromCustomerIdExclusive,
+                    customer -> appender.append(record(customer)));
+
+            return Optional.ofNullable(appender.isWriting() ? appender.getPath() : null);
+        }
+    }
+
+    public Optional<Path> exportForProductGroup(ProductGroup productGroup, Date fromDateInclusive, Date toDateExclusive) {
+        String exportName = "between-dates-dateA-dateB";
+
+        try(WelcomeLetterCsvExporter.Appender appender = csvExporter.createAppender(productGroup, exportName)) {
+            customerDao.scan(
+                    productGroup,
+                    fromDateInclusive,
+                    toDateExclusive,
+                    customer -> appender.append(record(customer)));
+
+            return Optional.ofNullable(appender.isWriting() ? appender.getPath() : null);
+        }
+    }
+
+    private CardOrderExportRecord record(Customer customer) {
+        return new CardOrderExportRecord.Builder(customer)
+                .address(customerAddress(customer))
+                .build();
+    }
+
+    private CustomerAddress customerAddress(Customer customer) {
+        return customerAddressDao
+                .findByCustomerId(customer.getCustomerId(), AddressType.BILLING)
+                .orElseGet(() -> customerAddressDao.findByCustomerId(customer.getCustomerId(), AddressType.MAIN).get());
+    }
+}
