d949cb78f96e67dcee74007e3d4204d3d8fd2867
Rui Salgado <ruivfsalgado@gmail.com>
2015-06-04 13:49:05 +0100

    Make it possible to load datasource from JNDI name #2520
    Name is configurable via prop datasource.jdniName.
    Defaults to “jdbc/pooledDS”.
    Name is looked up if datasource.url is missing.

--- modules/yellowbrick-data/src/main/java/nl/yellowbrick/data/bootstrap/DataSourceConfiguration.java
+++ modules/yellowbrick-data/src/main/java/nl/yellowbrick/data/bootstrap/DataSourceConfiguration.java
@@ -2,20 +2,29 @@ package nl.yellowbrick.data.bootstrap;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 @Configuration
 @EnableConfigurationProperties(DataSourceProperties.class)
 public class DataSourceConfiguration {
 
+    private static final String DEFAULT_JNDI_NAME = "jdbc/pooledDS";
+
     @Autowired
     private DataSourceProperties properties;
 
+    @ConditionalOnProperty("datasource.url")
     @Bean
     public DataSource dataSource() throws PropertyVetoException {
         ComboPooledDataSource ds = new ComboPooledDataSource();
@@ -25,5 +34,16 @@ public class DataSourceConfiguration {
         ds.setDriverClass(properties.driverClassName);
 
         return ds;
+    }
+
+    @ConditionalOnMissingBean
+    @ConditionalOnWebApplication
+    @Bean
+    public DataSource jndiDataSource() {
+        String name = isNullOrEmpty(properties.jdniName)
+                ? DEFAULT_JNDI_NAME
+                : properties.jdniName;
+
+        return new JndiDataSourceLookup().getDataSource(name);
     }
 }
